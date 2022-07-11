b133a1a157a323b03db45537c3b17f60c3e23536
Redwan-anik <redwan229900@gmail.com>
2022-07-12 01:19:37 +0600

    super updated

--- bin/super keyword.dart
+++ bin/super keyword.dart
@@ -1,5 +1,41 @@
 void main(){
 
+  var objForSon = son();
+  objForSon.checkSuper();
+//this is possible when its normal class
+//var objForuncle = uncle();
+//this is NotPossible when its Abstract class
+//var objForuncle = uncle();
+
+}
+
+
+// class uncle{
+//   void mult(){
+//     int a= 10;
+//     int b= 20;
+//     var c = b*a;
+//     print(c);
+//   }
+// }
+
+ class uncle{
+  var a=90;
+  void mult(){
+    int a= 10;
+    int b= 20;
+    var c = b*a;
+    print(c);
+  }
+}
+
+class son extends uncle{
+void checkSuper(){
+
+  super.mult();
+  print(super.a);
+
+}
 
 
 }
