ce4dd3ef5d75dc06ba4ca3f0c2ffd730e7e05104
md-redwanul-haque <redwan229900@gmail.com>
2024-05-14 17:06:49 +0600

    two sum

--- bin/ps/two_sum.dart
+++ bin/ps/two_sum.dart
@@ -1,11 +1,15 @@
 void main() {
 
-  List<int> myList = [1, 4, 6, 5, 9];
-  int target = 15;
+  List<int> myList = [2,7,11,15];
+  int target = 9;
 
 
   var obj = Solution();
   print(obj.twoSum(myList, target));
+  var obj2 = Solutions();
+  print(obj2.twoSum(myList, target));
+
+
 }
 
 
@@ -25,6 +29,52 @@ class Solution {
         result = [i, nums.indexOf(remaining)];
         break;
       }
+    }
+    return result;
+
+  }
+}
+
+class Solutions {
+
+  List twoSum(List nums, int target ){
+
+
+    var result=[];
+
+
+    for(int i=0;i<nums.length;i++){
+
+      for(int j=i+1;j<nums.length;j++){
+        if(nums[i]+nums[j]==target){
+          result.add(i);
+          result.add(j);
+
+        }
+      }
+
+    }
+    return result;
+
+  }
+}
+
+//my solve
+class Solutionss {
+
+  List twoSum(List nums, int target ){
+
+
+    var result=[];
+
+
+    for(int i=0;i<nums.length;i++){
+      var memoryData = target - nums[i];
+      if(nums.contains(memoryData) && i != nums.indexOf(memoryData) ){
+        result = [nums.indexOf(memoryData),i];
+        break;
+      }
+
     }
     return result;
 
